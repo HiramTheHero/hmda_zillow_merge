5e243c4ff8e5deb75c9a90491b84070259bb54ca
Daniel Hixson <dhixson@A-MacBook-Pro.local>
2023-03-22 15:54:52 -0600

    Adding HMDA subset file - In progress

--- scripts/2_hmda/0_procedure.sh
+++ scripts/2_hmda/0_procedure.sh
@@ -4,11 +4,8 @@ mkdir ./data/hmda_data/lar_data
 mkdir ./data/hmda_data/panel_zipped
 mkdir ./data/hmda_data/panel_data
 
-
-
 scripts/2_hmda/1_download.sh
 scripts/2_hmda/2_process_files.sh
 
 duckdb ./data/data.duckdb < scripts/2_hmda/3_load_lar.sql
-
 duckdb ./data/data.duckdb < scripts/2_hmda/3_load_panel.sql
