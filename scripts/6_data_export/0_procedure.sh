c02e13112d0393b0fb4d95371da51488407e5480
HiramTheHero <hixsonhd@gmail.com>
2023-03-29 16:52:33 -0600

    Fixed Export Command - Delimiters

--- scripts/6_data_export/0_procedure.sh
+++ scripts/6_data_export/0_procedure.sh
@@ -1,7 +1,7 @@
 mkdir data/MA/merged_data
 
-duckdb -s "COPY hmda_zillow_1994_2003 TO 'data/MA/merged_data/hmda_zillow_1994_2003.csv' (HEADER, DELIMITER ',|');" data/data.duckdb
-duckdb -s "COPY hmda_zillow_2004_2013 TO 'data/MA/merged_data/hmda_zillow_2004_2013.csv' (HEADER, DELIMITER ',|');" data/data.duckdb
-duckdb -s "COPY hmda_zillow_2014_2016 TO 'data/MA/merged_data/hmda_zillow_2014_2016.csv' (HEADER, DELIMITER ',|');" data/data.duckdb
-duckdb -s "COPY hmda_zillow_2017 TO 'data/MA/merged_data/hmda_zillow_2017.csv' (HEADER, DELIMITER ',|');" data/data.duckdb
-duckdb -s "COPY hmda_zillow_2018_2020 TO 'data/MA/merged_data/hmda_zillow_2018_2020.csv' (HEADER, DELIMITER ',|');" data/data.duckdb
+duckdb -s "COPY hmda_zillow_1994_2003 TO 'data/MA/merged_data/hmda_zillow_1994_2003.csv' (HEADER, DELIMITER '|');" data/data.duckdb
+duckdb -s "COPY hmda_zillow_2004_2013 TO 'data/MA/merged_data/hmda_zillow_2004_2013.csv' (HEADER, DELIMITER '|');" data/data.duckdb
+duckdb -s "COPY hmda_zillow_2014_2016 TO 'data/MA/merged_data/hmda_zillow_2014_2016.csv' (HEADER, DELIMITER '|');" data/data.duckdb
+duckdb -s "COPY hmda_zillow_2017 TO 'data/MA/merged_data/hmda_zillow_2017.csv' (HEADER, DELIMITER '|');" data/data.duckdb
+duckdb -s "COPY hmda_zillow_2018_2020 TO 'data/MA/merged_data/hmda_zillow_2018_2020.csv' (HEADER, DELIMITER '|');" data/data.duckdb
